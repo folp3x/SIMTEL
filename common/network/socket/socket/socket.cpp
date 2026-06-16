5166baa864809abc6af87dc09b7c1b88f11f4536
folp3x <gjghcr.nov@gmail.com>
2026-06-17 02:52:04 +0300

    Удалены отладочные сообщения из методов класса Socket

--- common/network/socket/socket/socket.cpp
+++ common/network/socket/socket/socket.cpp
@@ -6,9 +6,6 @@
 
 #include "common/network/socket/socket_message_header/socket_message_header.h"
 
-#include "common/utils/network/network.h"
-#include <iostream>
-
 namespace common {
 std::optional<NetworkError> Socket::sendAll(const void *data,
                                             size_t size) const {
@@ -92,7 +89,6 @@ bool Socket::setReceiveTimeout(int sock, unsigned int timeoutMsec) {
 }
 
 std::optional<NetworkError> Socket::sendMessage(const binary_t &data) const {
-  // std::cout << "SEND MESSAGE DATA: " << common::toStr(data) << std::endl;
   if (data.empty()) {
     return NetworkError{NetworkErrorType::EMPTY_MESSAGE, "Empty message"};
   }
@@ -142,9 +138,6 @@ std::expected<binary_t, NetworkError> Socket::receiveMessage() const {
         NetworkError{NetworkErrorType::INCOMPLETE_HEADER, "Incomplete header"});
   }
 
-  // std::cout << "RECEIVE MESSAGE HEADER: " << common::toStr(header) <<
-  // std::endl;
-
   auto in = zpp::bits::in(header);
   uint32_t msgSize = 0;
   if (in(msgSize) != zpp::bits::errc{}) {
@@ -153,8 +146,6 @@ std::expected<binary_t, NetworkError> Socket::receiveMessage() const {
   }
   msgSize = ntohl(msgSize);
 
-  // std::cout << "RECEIVE HEADER MSG SIZE: " << msgSize << std::endl;
-
   if (msgSize == 0) {
     return header;
   }
@@ -162,7 +153,6 @@ std::expected<binary_t, NetworkError> Socket::receiveMessage() const {
   // чтение данных
   binary_t content(msgSize);
   received = recv(sock, content.data(), msgSize, MSG_WAITALL | MSG_NOSIGNAL);
-  // std::cout << "RECEIVE CONTENT: " << common::toStr(content) << std::endl;
   if (received < 0)
     return std::unexpected(
         NetworkError{NetworkErrorType::OTHER, getLastError()});
